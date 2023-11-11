e3d8b3ba3fe0af451f728a3670f2959da6632442
Serhat <me.serhaterdem@gmail.com>
2023-11-11 21:15:13 +0300

    Initial commit

---
+++ app/src/main/java/com/erdemserhat/codeexplorer_listview/model/ProgrammingLanguage.kt
@@ -0,0 +1,3 @@
+package com.erdemserhat.codeexplorer_listview.model
+
+data class ProgrammingLanguage(val name:String,val description:String, val id:Int)
---
+++ app/src/main/java/com/erdemserhat/codeexplorer_listview/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.erdemserhat.codeexplorer_listview.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erdemserhat.codeexplorer_listview.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
